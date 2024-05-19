7bc9c9df878c72bed3691c1d77c31f8492879c81
Divyanshikhatri062 <divyanshikhatri182@gmail.com>
2024-05-19 10:37:09 +0530

    final changes made

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let msgc=document.querySelector(".msg-container");
 let msg=document.querySelector("#msg")
 
 let turno=true;
+let draw=0;
 const wp=[
     [0,1,2],
     [0,3,6],
@@ -15,34 +16,47 @@ const wp=[
     [3,4,5],
     [6,7,8],
 ];
-boxes.forEach((box)=>{
-    box.addEventListener("click",()=>{
-        console.log("box clicked");
-       if(turno){
-       box.innerText="O";
-       turno=false;
-       }
-       else{
-        box.innerText="X";
-        turno=true;
-       }
-       box.disabled=true;
-       cw();
-    });
-});
-const rgame=()=>{
+const rgame=()=>{//reset game
+    draw=0;
     turno=true;
     eb();
     msgc.classList.add("hide");
 };
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+       if(turno){//playerO
+       box.innerText="O";
+       turno=false;
+       }
+       else{//player X
+        box.innerText="X";
+        turno=true;
+       }
+       box.disabled=true;
+       draw++;
+       console.log("y");
+       console.log(draw);
+       let iswinner=cw();
+       if(draw===9 && !iswinner){
+        console.log("ey");
+        gameDraw();
+       }
+       
+    });
+});
+const gameDraw = () => {
+    msg.innerText = `Game was a Draw.`;
+    msgc.classList.remove("hide");
+    db();
+  };
 const eb=()=>{
-    for(let box in boxes){
+    for(let box of boxes){
         box.disabled=false;
         box.innerText="";
     }
 };
-const db=()=>{
-    for(let box in boxes){
+const db=()=>{//disable boxes
+    for(let box of boxes){
         box.disabled=true;
     }
 };
@@ -52,19 +66,17 @@ const sw=(winner)=>{
     db();
 };
 const cw=()=>{
-    for(pattern of wp){
+    for(let pattern of wp){
         let p1=boxes[pattern[0]].innerText;
         let p2=boxes[pattern[1]].innerText;
         let p3=boxes[pattern[2]].innerText;
         if(p1!="" && p2!="" && p3!=""){
             if(p1==p2 && p2==p3){
-                console.log("winner is",p1);
                 sw(p1);
+               return true;
             }
         }
     }
    };
-   const rg=()=>{
-    turno=true;
-
-   };
+   ngb.addEventListener("click", rgame);
+resetbtn.addEventListener("click", rgame);
